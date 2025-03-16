1a0fe58baedaa3168430f5759cd4f2bf3d1d4c0b
Vinicius de Ávila Bezerra <vinibezerra2004@gmail.com>
2025-03-15 21:01:39 -0300

    Create train and validade funcions
    Ainda não está funcionando corretamente, vou bater com o código da Nvidia para ver se faltou alguma coisa.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Adam
 
@@ -44,11 +45,74 @@ layers = [
 model = nn.Sequential(*layers)
 
 model.to(device)
-model = torch.compile(model)
+model.compile()
 
 loss_function = nn.CrossEntropyLoss()
 
 optimizer = Adam(model.parameters())
 
-# TODO: Continuar implementação
+# TODO: Treinar e validar o modelo
+
+train_N = len(train_loader.dataset)
+valid_N = len(valid_loader.dataset)
+
+def get_batch_accuracy(output: Tensor, y: Tensor, N):
+    pred = output.argmax(dim=1,keepdim=True)
+    correct = pred.eq(y.view_as(pred)).sum().item()
+    return correct / N
+
+def train():
+    loss = 0 
+    accuracy = 0
+
+    model.train()
+    for x, y in train_loader:
+        x, y = x.to(device), y.to(device)
+
+        output = model(x)
+        optimizer.zero_grad()
+        batch_loss = loss_function(output,y)
+        batch_loss.backward()
+        optimizer.step()
+
+        loss += batch_loss.item()
+        accuracy += get_batch_accuracy(output,y, train_N)
+    print('Train - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+
+def validate():
+    loss =0
+    accuracy = 0 
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in valid_loader:
+            x, y = x.to(device), y.to(device)
+            output = model(x)
+            
+            loss += loss_function(output,y).item()
+            accuracy += get_batch_accuracy(output,y, train_N)
+    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+
+if __name__ == "__main__":
+    epochs = 5
+
+    for epoch in range(epochs):
+        print("Epoch: {}".format(epoch))
+        train()
+        validate()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
